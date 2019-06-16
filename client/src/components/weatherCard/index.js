28f13ccf2a90b755544d665a65d31f590b8547cc
Gurpreet Singh <gurpreet.singh@preqin.com>
2019-06-16 20:20:38 +0100

    UI component- weather card

---
+++ client/src/components/weatherCard/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import uuid from 'uuid';
+
+const WeatherCard = (props) => {
+    const {nextFiveDays, name, current } = props; 
+    const description = current[0].description;
+
+    // filter out bad data
+    const filteredData = nextFiveDays.filter(day => day.date);
+
+    return (
+        <div className="weatherCard">
+            <div className="description">
+                <div>Currently in:</div>
+                <div className="city">{name}</div>
+                <div>{description}</div>
+            </div>
+            <div className="days">
+                {
+                    filteredData.map( day => 
+                        <div className="eachDay" key={uuid()} >
+                            <span className="dayName">{day.date}</span>
+                            <div>
+                                <span>Max: </span>
+                                <span>{day.tempMax}</span>
+                            </div>
+                            <div>
+                                <span>Min: </span>
+                                <span>{day.tempMin}</span>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
+} 
+
+export default WeatherCard;
