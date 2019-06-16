bfcd660726c3f7773c28f64fd029f9871d2d9ead
Gurpreet Singh <gurpreet.singh@preqin.com>
2019-06-16 18:55:09 +0100

    boilerplate, redux, sagas, axios, middleware etc

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './redux/reducers/rootReducer';
+import rootSaga from './redux/sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const reduxDevTools = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) || compose; 
+
+
+const store = createStore(
+        rootReducer,
+        compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+    );
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
